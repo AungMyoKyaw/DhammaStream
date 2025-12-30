3b73c5458307258d278a902287a11feb73730a09
Aung Myo Kyaw <aungmyokyaw@users.noreply.github.com>
2025-12-31 01:44:57 +0700

    fix: add fallback for BASE_PATH in production builds

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 			strict: false
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH || '/DhammaStream'
 		},
 		prerender: {
 			entries: ['/', '/teachers', '/browse'],
